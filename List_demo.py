50c11fb504e5076e464b937605e857d4a9cd2abb
Becky Blue <becky.bluedodge@gmail.com>
2017-05-01 14:53:24 -0700

    Add files via upload

---
+++ List_demo.py
@@ -0,0 +1,27 @@
+#Create the list of epic programmers
+epic_programmer_list = ["Jade Raymond",
+                        "Tracy Chou",
+                        "Grace Hopper",
+                        "Lady Ada Lovelace",
+                        "Leah Culver"]
+'''#print to console
+print "An epic programmer:" +epic_programmer_list[0]
+print "An epic programmer:" +epic_programmer_list[1]
+print "An epic programmer:" +epic_programmer_list[2]
+print "An epic programmer:" +epic_programmer_list[3]
+print "An epic programmer:" +epic_programmer_list[4]
+print "An epic programmer:" +epic_programmer_list[4]
+
+epic_programmer_list[4]= "Me"
+print epic_programmer_list
+
+#Add myself to the end of the list
+epic_programmer_list.append("Becky Blue")
+
+#Add this line to show myself in the colsole
+print "An epic programmer:" +epic_programmer_list[5]'''
+
+#Looping through each item in epic_programmer_list
+for programmer in epic_programmer_list:
+    #Print the programmers' name to console
+    print "An epic programmer:" + programmer
