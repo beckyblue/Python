206baf85f540b8a2e3887db3a27147bb2f298645
Becky Blue <becky.bluedodge@gmail.com>
2017-06-05 15:33:53 -0700

    Drill 63

---
+++ MoveAtoBfiles.py
@@ -0,0 +1,51 @@
+'''
+Using Python version 2.7
+Title: File Mover
+Scenario: Your employer wants a program to move all his .txt files from one folder to another
+with the click of a click of a button.
+On your desktop make 2 new folders.
+Call one Folder A & the second Folder B.
+Create 4 random .txt files & put them in Folder A.
+
+Plan:
+- Move the files from Folder A to Folder B.
+- Print out each file path that got moved onto the shell.
+- Upon viewing Folder A after the execution, the moved files should not be there.
+
+'''
+
+import shutil
+import os
+
+
+
+def moveFiles(folder):
+    if(folder == "A"):
+        source=os.listdir(os.path.abspath("/Users/Student/Desktop/Folder A/"))
+        destination = os.path.abspath("/Users/Student/Desktop/Folder B/")
+        folder = "/Users/Student/Desktop/Folder A/"
+    else:
+        source=os.listdir(os.path.abspath("/Users/Student/Desktop/Folder B/"))
+        destination = os.path.abspath("/Users/Student/Desktop/Folder A/")
+        folder = "/Users/Student/Desktop/Folder B/"
+
+    for files in source:
+        print ("Moved file:")
+        print (os.path.abspath(folder + files))
+        shutil.move(os.path.abspath(folder + files), destination)
+
+
+def main():
+    #See where the files are.
+    if(os.listdir("/Users/Student/Desktop/Folder A/")):
+        #files are in Folder A
+        moveFiles("A")
+    else:
+        #files are in Folder B
+        moveFiles("B")
+
+
+
+
+
+if __name__ == "__main__": main()
---
+++ ShellFileMove.py
@@ -0,0 +1,13 @@
+Python 2.7.13 (v2.7.13:a06454b1afa1, Dec 17 2016, 20:42:59) [MSC v.1500 32 bit (Intel)] on win32
+Type "copyright", "credits" or "license()" for more information.
+>>> 
+=============== RESTART: C:/Python27/Scripts/MoveAtoBfiles.py ===============
+Moved file:
+C:\Users\Student\Desktop\Folder A\Random1.txt
+Moved file:
+C:\Users\Student\Desktop\Folder A\Random2.txt
+Moved file:
+C:\Users\Student\Desktop\Folder A\Random3.txt
+Moved file:
+C:\Users\Student\Desktop\Folder A\Random4.txt
+>>> 
